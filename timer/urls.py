3cc8d3261bb713ed0070fb356fd27073c7777627
balu <balushow123@gmail.com>
2022-07-14 23:12:50 +1000

    ready proj before db

--- timer/urls.py
+++ timer/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from timer.views import WorkWithTimerView
+from timer.views import WorkWithTimerView, button_click
 
 urlpatterns = [
     path('', WorkWithTimerView.as_view(), name='home'),
+    path('button/', button_click, name='button'),
 ]
--- timer/views.py
+++ timer/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from datetime import timezone, datetime, timedelta, time
+
+from django.shortcuts import render, redirect
 from django.views import View
 
 from timer.models import Timer
@@ -7,8 +9,28 @@ from timer.models import Timer
 class WorkWithTimerView(View):
 
     def get(self, request):
-        return render(request, 'index.html')
+        last_obj = Timer.objects.all().last()
+        last_event = last_obj.event
+        cur_timer = last_obj.time
+        return render(request, 'index.html', {'event': last_event, 'timer': cur_timer})
 
-    def post(self, request):
-        print('123')
-        return render(request, 'index.html')
+
+def button_click(request):
+    last_obj = Timer.objects.all().last()
+    if not last_obj:
+        Timer.objects.create(time=time(), event='Start')
+    else:
+        minutes = 0
+        start_time = last_obj.date
+        time_zone = timezone(timedelta(hours=7))
+        seconds = int((datetime.now(time_zone) - start_time).total_seconds())
+        if seconds > 59:
+            minutes = seconds // 60
+            seconds %= 60
+        cur_timer = time(minute=minutes, second=seconds)
+        if last_obj.event == 'Start':
+            Timer.objects.create(time=cur_timer, event='Stop')
+        else:
+            Timer.objects.create(time=cur_timer, event='Start')
+
+    return redirect('home')
